5a8f0150f4710a350009a0b8e78bb400b3dcf593
github Doron <doron gurovich>
2021-10-18 15:12:27 +0300

    Week3 Day2 more Exercises

--- Week3/Day2/codeExercises.js
+++ Week3/Day2/codeExercises.js
@@ -37,11 +37,47 @@ var num1 = Number(prompt("please enter the first number"));
 var num2 = Number(prompt("please enter the second number"));
 var strOperator = prompt("please enter the operator");
 
+/*
 var math_it_up = {
     '+': function (x, y) { return x + y; },
     '-': function (x, y) { return x - y; }
 };​​​​​​​
-
+*/
 var numRes = 0; // math_it_up['+'](num1, num2);
 
 alert(numRes);
+
+// Exercises XP Ninja
+
+// Exercise 1 : Find Nemo
+
+var strNemo = prompt("please introduce Nemo");
+var intNemo = strNemo.indexOf("Nemo");
+
+if (intNemo<0) {
+    alert("your string doesnt contain Nemo");
+} else {
+    alert(intNemo);
+}
+
+// Exercise 3 : Ask For Numbers
+
+var strNumbers = prompt("please insert the string of numbers separated by commas. we will calculate the sum", "1,2,3,4,5,6,7,8,9");
+var arrNumbers = strNumbers.split(',');
+
+var sum = 0;
+let i = 0;
+
+while(i<arrNumbers.length) {
+    sum+= Number(arrNumbers[i++]);
+}
+
+alert("calculations results: " + sum);
+
+// Exercise 4 : Boom
+
+var exe4Input = Number(prompt("please insert the int number"));
+
+if (exe4Input < 2) {
+    alert("boom!");
+}8
