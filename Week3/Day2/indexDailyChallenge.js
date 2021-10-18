7e01a6bff2185563eac02862dd2858d877d63c73
github Doron <doron gurovich>
2021-10-18 09:31:01 +0300

    Week3 Day2 daily challenge JS arrays

---
+++ Week3/Day2/indexDailyChallenge.js
@@ -0,0 +1,25 @@
+
+// Exercise 1:
+
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift(); // Remove Banana from the array
+
+fruits.sort(); // Sort the array in alphabetical order.
+fruits.push("Kiwi"); // Add “Kiwi” to the end of the array.
+
+console.log(fruits);
+
+fruits = fruits.slice(1); // Remove “Apples” from the array. Don’t use the same method as in part 1.
+// delete fruits[0] - the array contain an empty element.
+
+fruits.reverse();// Sort the array in reverse order. (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
+
+console.log(fruits);
+
+// Exercise 2:
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+let strRes = moreFruits[1][1];
+
+console.log(strRes);
