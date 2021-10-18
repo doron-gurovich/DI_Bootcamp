6315190c873cec92ef7786be5967302faac8297c
github Doron <doron gurovich>
2021-10-18 12:43:27 +0300

    Week3 Day2 Exercises XP

---
+++ Week3/Day2/codeExercises.js
@@ -0,0 +1,6 @@
+var v1 = [(5 + "34"), (5 - "4"), (10 % 5), (5 % 10), ("Java" + "Script"), (" " + " "), (" " + 0), true + true, true + false, false + true, false - true, 3 - 4, "Bob" - "bill"];
+
+var v1Results = ["string", "int, 1", "Modulus (Remainder)", "Modulus (Remainder)", "string", "string", "string", "bool, 2","bool, 1", "bool, 1", "bool, -1","-1", "mistake"];
+
+console.log(v1);
+console.log(v1Results);
