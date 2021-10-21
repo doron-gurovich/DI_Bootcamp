400c76eff46daebc978f5cd9613cc991672ec9a6
github Doron <doron gurovich>
2021-10-21 14:19:34 +0300

    Week3 Day4 Exercises #3 Ninja updated. solved with brutal force

--- Week3/Day4/Exercises XP NINJA_3.js
+++ Week3/Day4/Exercises XP NINJA_3.js
@@ -3,16 +3,19 @@ var strCommand = prompt("please insert the action", "remove, replace");
 
 function funcVowels (str, command) {
     var pattern = /[aeiouy]/g;
+    var arrPattern = ['a','e','i','o','u','y'];
     var arrReplace = ['1','2','3','4','5','6'];
 
     if(!command.localeCompare("remove")) {
         return str.replace(pattern, '');
     } else if(!command.localeCompare("replace")) {
         
-        for(i = 0; i < str.length; i++) {
-            if(pattern.test(str[i])) {
-                str = str.replace(pattern, arrReplace[i]); // not yet working properly
-            }    
+        for(i = 0; i < str.length; i++) { // brutal force solution ... 
+            for(j = 0; j < arrPattern.length; j++) {
+                if(!str[i].localeCompare(arrPattern[j])) {
+                    str = str.replace(str[i], arrReplace[j]); 
+                }
+            }
         }
 
         return str;
