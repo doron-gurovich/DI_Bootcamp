c0a989f23d64bc6f12d9425268c7b191ec8cc028
github Doron <doron gurovich>
2021-10-21 16:11:55 +0300

    Week3 Day4 Daily challenge done

--- Week3/Day4/DailyChallenge-Not Bad.js
+++ Week3/Day4/DailyChallenge-Not Bad.js
@@ -1,21 +1,22 @@
-/*
+var strSentence = prompt("insert the tested string. need to include not ... bad", "The movie is not that bad, I like it");
+var strResult = strSentence;
 
-Instructions
-- Create a variable called sentence. The value of the variable should be a string that contains the words “not” and “bad”. For example, “The movie is not that bad, I like it”.
-- Create a variable called wordNot where it’s value is the first appearance of the substring “not” (from the sentence variable).
-- Create a variable called wordBad where it’s value is the first appearance of the substring “bad” (from the sentence variable).
-- If the word “bad” comes after the word “not”, you should replace the whole “not…bad” substring with “good”, then console.log the result.
-    For example, the result here should be : “The movie is good, I like it”
-    If the word “bad” does not come after “not” or the words are not in the sentence, console.log the original sentence.
+function funParcer(str) {
 
-*/
+    var iNot = (str.toLowerCase()).indexOf("not");
+    var iBad = (str.toLowerCase()).indexOf("bad");
 
-var strInput = prompt("insert the tested string. need to include not ... bad", "The movie is not that bad, I like it");
-
-function Not_Bad_Parcer(str) { // define if we have 'not', 'bad' or 'not ... bad' into the input string
-    if (str)
+    if(iNot == -1 && iBad == -1) {
+        return "the sentence you provide us doesn't contain 'not' or 'bad'";
+    } else if(iBad < iNot && iBad != -1) { // If the word “bad” does not come after “not” or the words are not in the sentence, console.log the original sentence.
+        return str;
+    } else if(iBad > iNot && iNot!= -1) { // If the word “bad” comes after the word “not”, you should replace the whole “not…bad” substring with “good”, then console.log the result.
+        return str.replace(str.substr(iNot, (iBad + 3 - iNot)), "good");
+    } else if(iBad < iNot && iBad == -1) { // i dont really understand what the question here ? what do you expect as an output ? its probably the same substr(), but from where we met 'not' till the end of the sentence ?
+        return str.substr(0, iNot);
+    } else if(iBad > iNot && iNot == -1) { // same question as the one above ... here i left the second part of the sentence. all after 'bad' (included)
+        return str.substr(iBad + 3);
+    }
 }
 
-var Not_Bad = {
-
-};
+console.log("output for your sentence (" + strSentence + ") is: \n" + funParcer(strSentence));
