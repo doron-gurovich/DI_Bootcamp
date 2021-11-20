73b8ceaae776fd3f1a4b36507aa2b12eef69e38e
github Doron <doron gurovich>
2021-11-20 19:49:33 +0200

    daily challenge

---
+++ Week7/Day4/Daily Challenge Sorting.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 20 18:21:43 2021
+
+@author: 97250
+"""
+
+def string_sorting( *args ):
+    
+    result = list(args).copy()
+    
+    for j in range(0, max(list(map(len, result)))):
+        
+        try:
+            iterator = [i for i in range(0, len(result))]
+            
+            input_list = list(zip(iterator, result))
+            
+            
+            sort_first_letter = {el[0]: el[1][j] for el in input_list if len(el[1]) > j}
+            sort_first_letter_res = {k: v for k, v in sorted(sort_first_letter.items(), key=lambda item: item[1])}
+            
+            temp_result = sort_first_letter_res.copy()        
+            temp_result.update(dict(input_list))
+            
+            result = [*temp_result.values()]
+            
+        except:
+            print("something went wrong")
+
+    return result
+
+print("sorted string: ", string_sorting("zxzx", "qwqw", "asas", "zxzx", "aassas", "aaasddsad"))
---
+++ Week7/Day4/Mini-Project - Tic Tac Toe.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def display_board(sizeX = 10, sizeY = 10):
+    
+
+    str_base = [str(i) for i in range(0, sizeX)]
+    
+    
+    
+
+    result = 0
+    print(result)
+
