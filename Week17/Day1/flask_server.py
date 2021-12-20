0dd7231462a17f82b2cae919a328ee958ebc6d7d
github Doron <doron gurovich>
2021-12-20 22:13:59 +0200

    drafts of main pages for FP were added. by now they are mostly empty

--- Week17/Day1/flask_server.py
+++ Week17/Day1/flask_server.py
@@ -9,9 +9,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/' or '/index.html')
 def index():
     return render_template('index.html')
+
+@app.route('/flow_chart.html')
+def flow_chart():
+    return render_template('flow_chart.html')
+
+@app.route('/procedure.html')
+def procedure():
+    return render_template('procedure.html')
+
+@app.route('/intro_project.html')
+def intro_project():
+    return render_template('intro_project.html')
+
+@app.route('/intro_press.html')
+def intro_press():
+    return render_template('intro_press.html')
+
+@app.route('/about_QDPS.html')
+def about_QDPS():
+    return render_template('about_QDPS.html')
 
 if __name__ == "__main__":
     app.run(port=5050, debug=True)
