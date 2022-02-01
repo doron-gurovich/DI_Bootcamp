9bf7a74bc4be05072c1ab4d9aa239c65e2d20fa6
github Doron <doron gurovich>
2022-02-01 10:38:36 +0200

    login page w/o connection to DB yet

---
+++ Week18/Day1/login_F-server/F-server.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, SubmitField
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+class InfoForm(FlaskForm):
+
+    position = StringField("What position you hold?")
+    submit = SubmitField('Submit')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+    position = False
+    form = InfoForm()
+    if form.validate_on_submit():
+        position = form.position.data
+        form.position.data = ''
+
+    return render_template('sign-up.html', form=form, position=position)
+
+@app.route('/thank-u')
+def thank_u():
+    first = request.args.get('first')
+    return render_template('thank-u.html', first=first)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
