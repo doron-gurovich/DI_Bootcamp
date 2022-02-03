76786bf4707f68ff4c50f459abc2bd19262cbd03
github Doron <doron gurovich>
2022-02-03 18:13:45 +0200

    POI connected to DB. very first draft included structure

---
+++ Week18/Day2/app.py
@@ -0,0 +1,93 @@
+222from POI_project import app, db
+from flask import Flask, render_template, request, redirect, session, url_for, flash, abort
+from flask_login import login_user, login_required, logout_user
+from flask_wtf import FlaskForm
+from wtforms import (Form, BooleanField, StringField, SubmitField,
+                    PasswordField, RadioField, validators,
+                    SelectField, TextAreaField)
+
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+from POI_project.models import User
+from POI_project.models import LoginForm, RegistrationForm
+form werkzeug.security import generate_password_hash, check_password_hash
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email= form.email.data).first()
+
+        if user.check_password(form.password.data) and user is not None:
+            login_user(user)
+            flash("Logged in Successfully!")
+
+            next = request.args.get('next')
+
+            if next == None or not next[0] == '/':
+                next = url_for('welcome')
+
+            return redirect(next)
+
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(email = form.email.data,
+                    username = form.username.data,
+                    password = form.password.data)
+        db.sessio.add(user)
+        db.session.commit()
+        flash("We are good! Thank you for the registration!")
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/welcome')
+@login_required
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You logged out. Thank you and see you soon!")
+    return render_template(url_for('home.html'))
+
+@app.route('/add-dish')
+def add_dish():
+    return render_template('add-dish.html')
+
+@app.route('/execute-order')
+def order():
+    return render_template('execute-order.html')
+
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
+@app.route('/introduce-order')
+def introduce_order():
+    return render_template('introduce-order.html')
+
+@app.route('/make-order')
+def make_order():
+    return render_template('make-order.html')
+
+@app.route('/share-lib')
+def share_lib():
+    return render_template('share-lib.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
